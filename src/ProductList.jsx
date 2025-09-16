2c8f3c4208fa558533c4ebc3c416210bc15d5f93
JAKe6699 <jondular72@yahoo.com>
2025-09-16 18:07:31 -0400

    some initial changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -253,34 +253,68 @@ function ProductList({ onHomeClick }) {
         setShowCart(false);
     };
     return (
-        <div>
-            <div className="navbar" style={styleObj}>
-                <div className="tag">
-                    <div className="luxury">
-                        <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/" onClick={(e) => handleHomeClick(e)}>
-                            <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
-                            </div>
-                        </a>
+        <>
+        <div className="navbar" style={styleObj}>
+            <div className="tag">
+                <div className="luxury">
+                    <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+                    <a href="/" onClick={(e) => handleHomeClick(e)}>
+                    <div>
+                        <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
+                        <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
                     </div>
-
-                </div>
-                <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    </a>
                 </div>
             </div>
-            {!showCart ? (
-                <div className="product-grid">
-
-
-                </div>
-            ) : (
-                <CartItem onContinueShopping={handleContinueShopping} />
-            )}
+            <div style={styleObjUl}>
+                <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+            </div>
         </div>
+
+        <div className="main_container">
+        {!showCart ? (
+            <div className="product-grid">
+            {plantsArray.map((item, index) => (
+                <div id="venue" className="plant_cat_container container_main">
+                     <div className="text">
+                        <h1>Plant Selection</h1>
+                    </div>
+                    <div className="plant_selection">
+                    <div className="plant_main" key={index}>
+                        <div className="img">
+                            <img src={item.img} alt={item.name} />
+                        </div>
+                        <div className="text">{item.name}</div>
+                        <div>${item.cost}</div>
+                        <div className="button_container">
+                            <>
+                            <button
+                                className={venueItems[index].quantity === 0 ? "btn-warning btn-disabled" : "btn-minus btn-warning"}
+                                onClick={() => handleRemoveFromCart(index)}
+                            >
+                                &#8211;
+                            </button>
+                            <span className="selected_count">
+                                {venueItems[index].quantity > 0 ? ` ${venueItems[index].quantity}` : "0"}
+                            </span>
+                            <button
+                                className={remainingAuditoriumQuantity === 0? "btn-success btn-disabled" : "btn-success btn-plus"}
+                                onClick={() => handleAddToCart(index)}
+                            >
+                                &#43;
+                            </button>
+                            </>
+                        </div> {/* button_container */}
+                    </div> {/* plant_selection */}
+                    </div> {/* plant_main */}
+                </div> {/* plant_cat_container */}
+            )}
+            </div> {/* product_grid */}
+        ) : (
+            <CartItem onContinueShopping={handleContinueShopping} />
+        )}
+        </div> {/* main container */}
     );
 }
 
